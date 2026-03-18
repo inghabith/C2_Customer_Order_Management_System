43b96e4f09633b9e40c93013deea7dc05ff77eb3
Habith <ing.hdeleon1@gmail.com>
2026-03-18 17:19:10 -0500

    changes

--- customer_register.py
+++ customer_register.py
@@ -3,26 +3,27 @@ def register_client(accumulated_tuple):
     
     while keep_registering == "yes":
 
-        print("customer registration".center(50, "="))
-        print("-" * 50)
+        print("CUSTOMER REGISTRATION".center(50, "="))
 
-        user_id = int(input("Enter your ID: "))
-        name = input("type your name: ")
-        email = input("enter your email: ")
+        user_id = int(input("Enter an ID: "))
+        name = input("Enter customer name: ")
+        email = input("Enter customer e-mail: ")
 
 
         customer = {
-            "nombre" : name,
+            "name" : name,
             "id"     : user_id,
             "email"  : email
         }
 
         accumulated_tuple = accumulated_tuple + (customer,)
+        print()
         print("-" * 50)
-        print("customer successfully registered✅".center(50, "="))
+        print("CUSTOMER SUCCESFULLY REGISTERED✅".center(50, "="))
         print("-" * 50)
+        print()
         keep_registering = input("Do you want to register another customer? yes/no: ").strip().lower()
+        print()
 
     return accumulated_tuple
     
-clients = register_client(())
