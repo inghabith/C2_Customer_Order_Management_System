e1253cc5190a546a51d0fcd8fa07b3a3060a0e8d
Habith <ing.hdeleon1@gmail.com>
2026-03-18 21:26:43 -0500

    Update FEATURE_CUSTOMER_REGISTER.py

--- FEATURE_CUSTOMER_REGISTER.py
+++ FEATURE_CUSTOMER_REGISTER.py
@@ -4,13 +4,34 @@ def register_client(accumulated_tuple):
     while keep_registering == "yes":
 
         print("CUSTOMER REGISTRATION".center(50, "="))
+        user_id = 0
+        while  user_id <=0 :
+            try:
+                user_id = int(input("Enter an ID: "))
 
-        user_id = int(input("Enter an ID: "))
-        name = input("Enter customer name: ")
-        email = input("Enter customer e-mail: ")
+                if user_id <= 0:
+                    print()
+                    print(f"{'❌ Error: Ingrese un valor positivo. ❌':^65}")
 
+            except ValueError:
+                print()
+                print(f"{'❌ Error: Ingrese un número entero válido. ❌':^65}")
 
-        customer = {
+        name = ""        
+        while  name == "": 
+                name = input("Customer name: ").strip()
+
+                if name == "":
+                    print(f"{'❌ Error: Este campo no puede estar vacio. ❌':^65}")
+
+        email = ""        
+        while  email == "": 
+                email = input("Customer e-mail: ").strip()
+
+                if email == "":
+                    print(f"{'❌ Error: Este campo no puede estar vacio. ❌':^65}")
+
+        customer = { 
             "name" : name,
             "id"     : user_id,
             "email"  : email
@@ -26,4 +47,3 @@ def register_client(accumulated_tuple):
         print()
 
     return accumulated_tuple
-    
