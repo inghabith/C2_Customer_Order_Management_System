bae9390997bdb0512f8e1ab3eb9e16ab1704fd0a
Habith <ing.hdeleon1@gmail.com>
2026-03-18 20:46:04 -0500

    Update FEATURE_PRODUCT_REGISTER.py

--- FEATURE_PRODUCT_REGISTER.py
+++ FEATURE_PRODUCT_REGISTER.py
@@ -9,11 +9,43 @@ def register_products(products):
 
     while keep_registering == "yes":
 
-        product_id = int(input("Enter product ID: "))
+#validacion de datos para cada input "product_name, product_price"
+        product_id = 0
+        while  product_id <=0 :
+            try:
+                product_id = int(input("Enter product ID: "))
 
-        
-        product_name = input("Enter product name: ")
-        product_price = int(input("Enter unit price: $"))
+                if product_id <= 0:
+                    print()
+                    print(f"{'❌ Error: Ingrese un valor positivo. ❌':^65}")
+
+            except ValueError:
+                print()
+                print(f"{'❌ Error: Ingrese un número entero válido. ❌':^65}")
+               
+# validation on products name.
+        product_name = ""
+        while  product_name == "": 
+                
+                product_name = input("Enter product name: ").strip()
+
+                if product_name == "":
+                    print(f"{'❌ Error: Este campo no puede estar vacio. ❌':^65}")
+
+# ===== Validation on  product_price 
+        product_price = 0
+        while  product_price <=0:
+            try:
+                product_price = int(input("Enter unit price: $"))
+
+                if product_price <= 0:
+                    print("="*60)
+                    print(f"{'❌ Error: Ingrese un valor positivo. ❌':^65}")
+                    print("="*60)
+
+            except ValueError:
+                print()
+                print(f"{'❌ Error: Ingrese un número entero válido. ❌':^65}")
 
         dictionary = {
                 "id": product_id,
@@ -23,13 +55,9 @@ def register_products(products):
 
         products = products + (dictionary,)
 
-        keep_registering = input(
-            "Would you like to enter another product? yes/no: "
-        ).strip().lower()
+        keep_registering = input("Would you like to enter another product? yes/no: " ).strip().lower()
 
     return products
-
-
 
 
 
